60311d43200a22567d9c607945cf7d428829f5df
Abu Shaid Sabbir <75726900+sabbir102629@users.noreply.github.com>
2021-03-02 01:33:08 -0800

    final 1

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["YouTuber", "Developer", "Programmer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
