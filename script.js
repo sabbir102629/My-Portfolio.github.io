46597002bf8ccb37b97c7944e7af9176f46a43cd
Abu Shaid Sabbir <75726900+sabbir102629@users.noreply.github.com>
2021-03-02 01:32:13 -0800

    final.0

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["YouTuber", "Developer", "Programmer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
